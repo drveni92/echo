5ba15839e43f6b4540ae1d6342e06569ca170671
AmerRatkovic <aratkovic2@etf.unsa.ba>
2017-03-08 10:05:23 +0100

    GET Shippers

---
+++ Billing.API/Models/ShipperModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Billing.API.Models
+{
+    public class ShipperModel
+    {
+        public ShipperModel()
+        {
+            InvoicesNo = new List<string>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+        public List<string> InvoicesNo { get; internal set; }
+    }
+}

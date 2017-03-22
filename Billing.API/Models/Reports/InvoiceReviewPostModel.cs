5b9c6360cf85079b6819dc4b017fca164c0d30b8
AmerRatkovic <aratkovic2@etf.unsa.ba>
2017-03-22 10:45:32 +0100

    Invoice Review Post

---
+++ Billing.API/Models/Reports/InvoiceReviewPostModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Billing.API.Models.Reports
+{
+    public class InvoiceReviewModel
+    {
+     
+        public int InvoiceId { get; set; }
+        public string InvoiceNo { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime? ShippedOn { get; set; }
+        public double InvoiceTotal { get; set; }
+        public string InvoiceStatus { get; set; }
+    }
+
+    public class InvoiceReviewCustomerModel
+    {
+        public InvoiceReviewCustomerModel()
+        {
+            Invoices = new List<InvoiceReviewModel>();
+        }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double GrandTotal { get; set; }
+        public List<InvoiceReviewModel> Invoices {get; set;}
+
+    }
+
+}

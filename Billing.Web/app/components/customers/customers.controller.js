325789e5dbee7d0c26bbf5e25db1f1c044fdfb2f
AmarBasic <amarbasic4@gmail.com>
2017-04-03 22:07:44 +0200

    ui fixed - sidebar menu and main content align; get all customers finished - no pagination

--- Billing.Web/app/components/customers/customers.controller.js
+++ Billing.Web/app/components/customers/customers.controller.js
@@ -1,49 +1,49 @@
-/**
- * Created by User on 31.03.2017..
- */
-(function(){
-
-    var app = angular.module("Billing");
-
-    var CustomersController = function($scope, $http, DataFactory) {
-
-        $scope.showCustomer = false;
-        ListCustomers();
-
-        $scope.getCustomer = function(currentCustomer){
+angular
+    .module("Billing")
+    .controller('CustomersController', ['$scope', '$http', 'DataFactory',  function($scope, $http, DataFactory) {
+        $scope.get = function(currentCustomer) {
             $scope.customer = currentCustomer;
             $scope.showCustomer = true;
         };
 
-        $scope.saveC = function(){
-            if($scope.customer.id === 0)
-                DataFactory.insert("customers", $scope.customer, function(data){ ListCustomers();} );
+        $scope.save = function() {
+            if ($scope.customer.id === 0)
+                DataFactory.insert("customers", $scope.customer, function(data) { ListCustomers(); });
             else
-                DataFactory.update("customers", $scope.customer.id, $scope.customer, function(data){ListCustomers();});
+                DataFactory.update("customers", $scope.customer.id, $scope.customer, function(data) { ListCustomers(); });
         };
-        $scope.deleteC = function(){
+        $scope.delete = function() {
 
-            DataFactory.delete("customers", $scope.customer.id, function(data){ListCustomers();});
+            DataFactory.delete("customers", $scope.customer.id, function(data) { ListCustomers(); });
             $scope.showAgent = false;
             $scope.showCustomer = false;
 
         };
 
-        $scope.new = function(){
+        $scope.new = function() {
             $scope.customer = {
                 id: 0,
                 name: "",
-                address:"",
-                town : {id:1}
+                address: "",
+                town: { id: 1 }
             };
-            $scope.showCustomer = true;
         };
 
-        function ListCustomers(){
-            DataFactory.list("customers", function(data){ $scope.customers = data});
+        $scope.open = function() {
+            $scope.showModal = true;
+        };
+
+        $scope.ok = function() {
+            $scope.showModal = false;
+        };
+
+        $scope.cancel = function() {
+            $scope.showModal = false;
+        };
+
+        function ListCustomers() {
+            DataFactory.list("customers", function(data) { $scope.customers = data });
         }
-    };
 
-    app.controller("CustomersController", CustomersController);
-
-}());
+        ListCustomers();
+    }]);

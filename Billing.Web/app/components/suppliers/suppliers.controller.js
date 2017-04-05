34eec767a235faa64a7fa5e098c607a5898a648c
drveni92 <drveni92@gmail.com>
2017-04-05 15:02:40 +0200

    supplier and categories crud all, problem with editing town

--- Billing.Web/app/components/suppliers/suppliers.controller.js
+++ Billing.Web/app/components/suppliers/suppliers.controller.js
@@ -1,23 +1,11 @@
 angular
     .module("Billing")
     .controller('SuppliersController', ['$scope', '$http', '$uibModal', 'DataFactory', function($scope, $http, $uibModal, DataFactory) {
-        $scope.get = function(currentSupplier) {
-            $scope.supplier = currentSupplier;
-            $scope.showSupplier = true;
+
+        $scope.delete = function() {
+            DataFactory.delete("suppliers", $scope.supplier.id, function(data) { ListSuppliers(); });
         };
 
-        $scope.save = function(){
-            if($scope.supplier.id === 0)
-                DataFactory.insert("supliers", $scope.supplier, function(data){ ListSuppliers();} );
-            else
-                DataFactory.update("supliers", $scope.supplier.id, $scope.supplier, function(data){ListSuppliers();});
-        };
-        $scope.delete = function(){
-
-            DataFactory.delete("suppliers", $scope.supplier.id, function(data){ListSuppliers();});
-            $scope.showAgent = false;
-
-        };
         function ListSuppliers() {
             DataFactory.list("suppliers", function(data) { $scope.suppliers = data });
         }
@@ -25,29 +13,88 @@ angular
         ListSuppliers();
 
         $scope.new = function() {
-    DataFactory.list("towns", function(data) {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'app/components/suppliers/templates/new.html',
-            controller: 'ModalInstanceController',
-            controllerAs: '$modal',
-            resolve: {
-                data: function() {
-                    return { id: 0, name: '', address: '', town: { id: null } }
-                },
-                options: function() {
-                    return { towns: data }
-                }
-            }
-        });
-        modalInstance.result.then(function(supplier) {
-            DataFactory.insert("suppliers", supplier, function(data) { ListSuppliers(); });
-        }, function() {
-            console.log('Modal dismissed at: ' + new Date());
-        });
+            DataFactory.list("towns", function (data) {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'app/components/suppliers/templates/new.html',
+                    controller: 'ModalInstanceController',
+                    controllerAs: '$modal',
+                    resolve: {
+                        data: function () {
+                            return {id: 0, name: '', address: '', town: {id: null}}
+                        },
+                        options: function () {
+                            return {towns: data}
+                        }
+                    }
+                });
+                modalInstance.result.then(function (supplier) {
+                    DataFactory.insert("suppliers", supplier, function (data) {
+                        ListSuppliers();
+                    });
+                }, function () {
+                    console.log('Modal dismissed at: ' + new Date());
+                });
 
-    });
-};
+            });
+        };
+            $scope.edit = function(supplier) {
+                DataFactory.list("towns", function(data) {
+                    var modalInstance = $uibModal.open({
+                        animation: true,
+                        ariaLabelledBy: 'modal-title',
+                        ariaDescribedBy: 'modal-body',
+                        templateUrl: 'app/components/suppliers/templates/edit.html',
+                        controller: 'ModalInstanceController',
+                        controllerAs: '$modal',
+                        resolve: {
+                            data: function() {
+                                return supplier
+                            },
+                            options: function() {
+                                return { towns: data }
+                            }
+                        }
+                    });
+
+                    modalInstance.result.then(function(supplier) {
+                        DataFactory.update("suppliers", supplier.id, supplier, function(data) {
+                            //message success missing
+                        });
+                    }, function() {
+                        console.log('Modal dismissed at: ' + new Date());
+                    });
+                });
+            }
+
+        $scope.delete = function(supplier) {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'app/components/suppliers/templates/delete.html',
+                controller: 'ModalInstanceController',
+                controllerAs: '$modal',
+                resolve: {
+                    data: function() {
+                        return supplier
+                    },
+                    options: function() {
+                        return null
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(supplier) {
+                DataFactory.delete("suppliers", supplier.id, function(data) {
+                    ListSuppliers();
+                    //message success missing
+                });
+            }, function() {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        }
+
 }]);

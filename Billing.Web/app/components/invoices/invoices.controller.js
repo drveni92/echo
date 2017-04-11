8a88ae37a7c4b2a3254f12bc9f032c2050f69b9c
drveni92 <drveni92@gmail.com>
2017-04-11 15:59:57 +0200

    history report finished

--- Billing.Web/app/components/invoices/invoices.controller.js
+++ Billing.Web/app/components/invoices/invoices.controller.js
@@ -40,6 +40,7 @@
                     templateUrl: 'app/components/invoices/templates/show_histories.html',
                     controller: 'ModalInstanceController',
                     controllerAs: '$modal',
+                    scope: $scope,
                     size: 'lg',
                     resolve: {
                         data: function() {

0ffad5a7e416fbd41e331a2d78cf2484ee7f5237
AmarBasic <amarbasic4@gmail.com>
2017-04-13 09:20:54 +0200

    fix messages for response in data factory

--- Billing.Web/app/factory/data.factory.js
+++ Billing.Web/app/factory/data.factory.js
@@ -14,56 +14,47 @@
 
             list: function(dataSet, callback) {
                 $http.get(source + dataSet)
-                    .success(function (data, status, headers) {
-                        return callback(data);
-                    })
-                    .error(function (error) {
-                        ToasterService.pop('error', "Error", error.message);
-                        return callback(false);
+                    .then(function success(response) {
+                        return callback(response.data);
+                    }, function error(error) {
+                        ToasterService.pop('error', "Error", error.statusText);
                     });
+
             },
 
             read: function(dataSet, id, callback) {
                 $http.get(source + dataSet + "/" + id)
-                    .success(function(data) {
-                        return callback(data);
-                    })
-                    .error(function(error) {
-                        ToasterService.pop('error', "Error", error.message);
-                        return callback(false);
+                    .then(function success(response) {
+                        return callback(response.data);
+                    }, function error(error) {
+                        ToasterService.pop('error', "Error", error.statusText);
                     });
             },
 
             insert: function(dataSet, data, callback) {
-                $http({ method:"post", url:source + dataSet, data:data })
-                    .success(function(data) {
-                        return callback(data);
-                    })
-                    .error(function(error){
-                        ToasterService.pop('error', "Error", error.message);
-                        return callback(false);
+                $http({ method: "post", url: source + dataSet, data: data })
+                    .then(function success(response) {
+                        return callback(response.data);
+                    }, function error(error) {
+                        ToasterService.pop('error', "Error", error.statusText);
                     });
             },
 
             update: function(dataSet, id, data, callback) {
-                $http({ method:"put", url:source + dataSet + "/" + id, data: data })
-                    .success(function(data) {
-                        return callback(data);
-                    })
-                    .error(function(error){
-                        ToasterService.pop('error', "Error", error.message);
-                        return callback(false);
+                $http({ method: "put", url: source + dataSet + "/" + id, data: data })
+                    .then(function success(response) {
+                        return callback(response.data);
+                    }, function error(error) {
+                        ToasterService.pop('error', "Error", error.statusText);
                     });
             },
 
             delete: function(dataSet, id, callback) {
-                $http({ method:"delete", url:source + dataSet + "/" + id })
-                    .success(function() {
-                        return callback(true);
-                    })
-                    .error(function(error){
-                        ToasterService.pop('error', "Error", error.message);
-                        return callback(false);
+                $http({ method: "delete", url: source + dataSet + "/" + id })
+                    .then(function success(response) {
+                        return callback(response.data);
+                    }, function error(error) {
+                        ToasterService.pop('error', "Error", error.statusText);
                     });
             }
         };

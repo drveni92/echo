42d0580d6e8a465640c46959ba87d9e9134f415a
AmarBasic <amarbasic4@gmail.com>
2017-05-10 14:30:40 +0200

    soft delete with interceptor

---
+++ Billing.Database/BillingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Billing.Database.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billing.Database
+{
+    public class BillingConfiguration : DbConfiguration
+    {
+        public BillingConfiguration()
+        {
+            AddInterceptor(new SoftDeleteInterceptor());
+        }
+    }
+}

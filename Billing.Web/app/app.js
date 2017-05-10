89d0ffad1c866e8df3480dabecbb79fd8ede3ab5
AmarBasic <amarbasic4@gmail.com>
2017-05-10 10:11:35 +0200

    remove admin access duplications

--- Billing.Web/app/app.js
+++ Billing.Web/app/app.js
@@ -128,20 +128,4 @@
         });
         $rootScope.authenticated = authenticated;
     });
-
-    app.directive('adminAccess', function () {
-        return {
-            scope: { owner: '=' },
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                var access = (credentials.currentUser.roles.indexOf("admin") > -1);
-                if (!access) {
-                    var user = credentials.currentUser.id;
-                    var owner = scope.owner;
-                    if (owner != user) element.remove();
-                }
-            }
-        };
-    });
-
 }());
